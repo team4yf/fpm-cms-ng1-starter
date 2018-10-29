632a6877fc195f61731fa5b5d38ea979c602aa52
yfsoftcom <yfsoftcom@126.com>
2018-10-29 09:43:31 +0800

    debug the took

--- tool/generate.js
+++ tool/generate.js
@@ -1,38 +1,44 @@
 const _ = require('lodash');
 const fs = require('fs');
+const path = require('path');
+
+const table_metas = _.map(_.remove(process.argv, arg => {
+                              return arg.endsWith('.json')
+                            }),
+                            json_path => {
+                              return require(path.join(__dirname, json_path))
+                            });
 
 const AI_START = 1;
-const TABLE_COMMENT = 'test';
-const TABLE_NAME = 't_test';
 
-const fields = [
-  {
-    name: 'name',
-    type: 'varchar(200)',
-    nn: 'NOT',
-    dft: 'NULL',
-    comment: '名称',
-  }
-];
-const field_compiled = _.template('\`${ name }\` ${ type } ${ nn } NULL DEFAULT ${ dft } COMMENT \'${ comment }\', ');
-const FIELDS = _.reduce( fields, ( r, i ) => {
-  return r + field_compiled(i)
-}, '');
-
-const SQL_TEMPLATE = `
-DROP TABLE IF EXISTS \`${ TABLE_NAME }\`;
-CREATE TABLE IF NOT EXISTS \`${ TABLE_NAME }\` (
+const generate_one = meta => {
+  const { name, comment = '-', fields } = meta;
+  const field_compiled = _.template('\`${ name }\` ${ type } ${ nn } NULL ${ dft } COMMENT \'${ comment }\', ');
+  const FIELDS = _.reduce( fields, ( r, i ) => {
+    const f = Object.assign({ nn: '', dft: '', comment: '-'}, i)
+    if(!_.isEmpty(f.dft)){
+      f.dft = 'DEFAULT ' + (_.isString(f.dft)? `'${f.dft}'` : f.dft)
+    }
+    return r + '  ' + field_compiled(f) + '\n'
+  }, '');
+  const SQL_TEMPLATE = `
+DROP TABLE IF EXISTS \`${ name }\`;
+CREATE TABLE IF NOT EXISTS \`${ name }\` (
   \`id\` bigint(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
   \`delflag\` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除标示',
   \`createAt\` bigint(20) NOT NULL DEFAULT '0' COMMENT '数据创建时间戳',
   \`updateAt\` bigint(20) NOT NULL DEFAULT '0' COMMENT '数据更新时间戳',
-  ${ FIELDS }
-  PRIMARY KEY (\`id\`)
-) ENGINE=InnoDB AUTO_INCREMENT=${ AI_START } DEFAULT CHARSET=utf8 COMMENT='${ TABLE_COMMENT }';
-`;
-console.log(SQL_TEMPLATE);
-// return;
-fs.writeFile(`${ TABLE_NAME }.sql`, SQL_TEMPLATE, (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
+${ FIELDS }  PRIMARY KEY (\`id\`)
+) ENGINE=InnoDB AUTO_INCREMENT=${ AI_START } DEFAULT CHARSET=utf8 COMMENT='${ comment }';
+  `;
+  return SQL_TEMPLATE;
+}
+const sqls = _.map(table_metas, meta => {
+  return generate_one(meta);
+})
+_.map(sqls, sql => console.log(sql));
+
+// fs.writeFile(`${ TABLE_NAME }.sql`, SQL_TEMPLATE, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
