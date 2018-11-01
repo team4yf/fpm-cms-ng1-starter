bd6bf59619d2a31f2f39d59a8de459c0b22346f2
丁明亮 <1805334129@qq.com>
2018-11-01 17:28:35 +0800

    datameta

--- public/js/controller/setting.js
+++ public/js/controller/setting.js
@@ -4,23 +4,27 @@ angular.module('fpm.c.setting', ['fpm.service', 'fpm.filter'])
     function ($scope, $ngFpmcService) {
         // console.log('aaa', $ngFpmcService);
         $scope.user = {
-          nikename : '',
-          login_id : '',
-          login_pass : '',
+          nickname : '',
+          password : '',
           mobile : ''
         };
-        console.log($scope.nikename);
         $scope.save = function () {
-          // const { Func, Obj } = $ngFpmcService;
-          // var obj = new Obj('cms_setting');
-          //   obj.set($scope.user)
-          //       .create()
-          //       .then(function(data){
-          //         console.info(data);
-          //       }).catch(function(err){
-          //         console.error(err);
-          //       });
-          console.log($scope.user);
+          if($scope.settingForm.$invalid){
+            return false;
+          }else{
+            const { Func, Obj } = $ngFpmcService;
+            var obj = new Obj('cms_setting');
+              obj.set($scope.user)
+                  .create()
+                  .then(function(data){
+                    alert('注册成功');
+                    location.reload();
+                    console.log(data);
+                  }).catch(function(err){
+                    console.error(err);
+                  });
+            console.log($scope.user);
+          }
         }
         
     }])
