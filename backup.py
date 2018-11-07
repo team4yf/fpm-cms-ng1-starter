2841e75a864dd44c52867014c70382188aad4ff9
yfsoftcom <yfsoftcom@126.com>
2018-11-07 14:17:51 +0800

    upgrade

--- backup.py
+++ backup.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import datetime
 now = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')
-# docker exec -it fpm-cms-ng1-starter_db_1 mysqldump -uroot -proot fpm_project > backup/fpm_project.sql
-output = os.popen('docker exec -it fpm-cms-ng1-starter_db_1 mysqldump -uroot -proot fpm_project > backup/' + now + '.sql')
+project = os.path.split(os.getcwd())[-1]  # 获取当前文件夹的名称，用来找到 docker 中的容器
+project = project.replace('.', '')  # docker生成的容器会去除目录中的 . 号，这里做一个替换
+output = os.popen('docker exec -it ' + project + '_db_1 mysqldump -uroot -proot fpm_project > backup/' + now + '.sql')
 print(output.read())
-
