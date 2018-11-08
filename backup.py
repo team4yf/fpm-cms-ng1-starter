f5c83d4f555e69ad1227963a014b5c1d5c2a5aca
yfsoftcom <yfsoftcom@126.com>
2018-11-08 22:23:25 +0800

    change docker container_name

--- backup.py
+++ backup.py
@@ -5,5 +5,5 @@ import datetime
 now = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M')
 # project = os.path.split(os.getcwd())[-1]  # 获取当前文件夹的名称，用来找到 docker 中的容器
 # project = project.replace('.', '')  # docker生成的容器会去除目录中的 . 号，这里做一个替换
-output = os.popen('docker exec -it docker_db_1 mysqldump -uroot -proot fpm_project > backup/' + now + '.sql')
+output = os.popen('docker exec -it mysql-server mysqldump -uroot -proot fpm_project > backup/' + now + '.sql')
 print(output.read())
